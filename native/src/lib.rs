c2681a92da1f2bbd33908dd041e8eaf3cc301714
Krzysztof Zawisła <32432944+KrzysztofZawisla@users.noreply.github.com>
2019-12-07 17:16:47 +0100

    Optimized code

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -8,115 +8,77 @@ use colorful::Colorful;
 use std::fs;
 use std::path;
 
-fn console_info(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+fn console_output(mut cx: FunctionContext, type &str) -> () {
     let now = chrono::Local::now();
     let message = cx.argument::<JsString>(0)?.value();
     let now = now.format("[%Y-%m-%d %H:%M:%S%.3f] ").to_string();
-    let output = now+"[info] "+&message;
-    println!("{}", output);
+    let output = now+"["+type+"] "+&message;
+    if type == "info" {
+        println!("{}", output);
+    } else if type == "error" {
+        println!("{}", output.color(Color::Red));
+    } else if type == "success" {
+        println!("{}", output.color(Color::Green));
+    } else if type == "warning" {
+        println!("{}", output.color(Color::Orange1));
+    }
+}
+
+fn file_output(mut cx: FunctionContext, type &str) -> () {
+    let now = chrono::Local::now();
+    let message = cx.argument::<JsString>(0)?.value();
+    let path = cx.argument::<JsString>(1)?.value();
+    let now = now.format("[%Y-%m-%d %H:%M:%S%.3f] ").to_string();
+    let output = now+"["+type+"] "+&message;
+    if !path::Path::new(&path).exists() {
+        fs::File::create(&path).expect("Cannot create log file");
+    }
+    let log_file_content = fs::read_to_string(&path).expect("Cannot read log file");
+    let mut new_line = "";
+    if log_file_content != "" {
+        new_line = "\n";
+    }
+    let log_file_content = log_file_content+new_line+&output;
+    fs::write(path, log_file_content).expect("Cannot write to log file");
+}
+
+fn console_info(mut cx: FunctionContext) -> JsResult<JsUndefined> {
+    console_output(cx, "info");
     Ok(cx.undefined())
 }
 
 fn console_error(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let now = chrono::Local::now();
-    let message = cx.argument::<JsString>(0)?.value();
-    let now = now.format("[%Y-%m-%d %H:%M:%S%.3f] ").to_string();
-    let output = now+"[error] "+&message;
-    println!("{}", output.color(Color::Red));
+    console_output(cx, "error");
     Ok(cx.undefined())
 }
 
 fn console_success(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let now = chrono::Local::now();
-    let message = cx.argument::<JsString>(0)?.value();
-    let now = now.format("[%Y-%m-%d %H:%M:%S%.3f] ").to_string();
-    let output = now+"[success] "+&message;
-    println!("{}", output.color(Color::Green));
+    console_output(cx, "success");
     Ok(cx.undefined())
 }
 
 fn console_warn(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let now = chrono::Local::now();
-    let message = cx.argument::<JsString>(0)?.value();
-    let now = now.format("[%Y-%m-%d %H:%M:%S%.3f] ").to_string();
-    let output = now+"[success] "+&message;
-    println!("{}", output.color(Color::Orange1));
+    console_output(cx, "warning");
     Ok(cx.undefined())
 }
 
 fn file_info(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let now = chrono::Local::now();
-    let message = cx.argument::<JsString>(0)?.value();
-    let path = cx.argument::<JsString>(1)?.value();
-    let now = now.format("[%Y-%m-%d %H:%M:%S%.3f] ").to_string();
-    let output = now+"[info] "+&message;
-    if !path::Path::new(&path).exists() {
-        fs::File::create(&path).expect("Cannot create log file");
-    }
-    let log_file_content = fs::read_to_string(&path).expect("Cannot read log file");
-    let mut new_line = "";
-    if log_file_content != "" {
-        new_line = "\n";
-    }
-    let log_file_content = log_file_content+new_line+&output;
-    fs::write(path, log_file_content).expect("Cannot write to log file");
+    file_output(cx, "info");
     Ok(cx.undefined())
 }
 
 fn file_success(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let now = chrono::Local::now();
-    let message = cx.argument::<JsString>(0)?.value();
-    let path = cx.argument::<JsString>(1)?.value();
-    let now = now.format("[%Y-%m-%d %H:%M:%S%.3f] ").to_string();
-    let output = now+"[success] "+&message;
-    if !path::Path::new(&path).exists() {
-        fs::File::create(&path).expect("Cannot create log file");
-    }
-    let log_file_content = fs::read_to_string(&path).expect("Cannot read log file");
-    let mut new_line = "";
-    if log_file_content != "" {
-        new_line = "\n";
-    }
-    let log_file_content = log_file_content+new_line+&output;
-    fs::write(path, log_file_content).expect("Cannot write to log file");
+    file_output(cx, "success");
     Ok(cx.undefined())
 }
 
 fn file_error(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let now = chrono::Local::now();
-    let message = cx.argument::<JsString>(0)?.value();
-    let path = cx.argument::<JsString>(1)?.value();
-    let now = now.format("[%Y-%m-%d %H:%M:%S%.3f] ").to_string();
-    let output = now+"[error] "+&message;
-    if !path::Path::new(&path).exists() {
-        fs::File::create(&path).expect("Cannot create log file");
-    }
-    let log_file_content = fs::read_to_string(&path).expect("Cannot read log file");
-    let mut new_line = "";
-    if log_file_content != "" {
-        new_line = "\n";
-    }
-    let log_file_content = log_file_content+new_line+&output;
-    fs::write(path, log_file_content).expect("Cannot write to log file");
+    file_output(cx, "error");
     Ok(cx.undefined())
 }
 
 fn file_warn(mut cx: FunctionContext) -> JsResult<JsUndefined> {
-    let now = chrono::Local::now();
-    let message = cx.argument::<JsString>(0)?.value();
-    let path = cx.argument::<JsString>(1)?.value();
-    let now = now.format("[%Y-%m-%d %H:%M:%S%.3f] ").to_string();
-    let output = now+"[warn] "+&message;
-    if !path::Path::new(&path).exists() {
-        fs::File::create(&path).expect("Cannot create log file");
-    }
-    let log_file_content = fs::read_to_string(&path).expect("Cannot read log file");
-    let mut new_line = "";
-    if log_file_content != "" {
-        new_line = "\n";
-    }
-    let log_file_content = log_file_content+new_line+&output;
-    fs::write(path, log_file_content).expect("Cannot write to log file");
+    file_output(cx, "warn");
     Ok(cx.undefined())
 }
 
